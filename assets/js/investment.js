639279fce4331ac7d4936121d9872216cab74ff3
Rajan Dubey <rajan@binmile.com>
2023-08-02 14:02:26 +0530

    investment new change

--- assets/js/investment.js
+++ assets/js/investment.js
@@ -55,3 +55,42 @@ function redirectPage() {
     }
 }
 
+
+function show1(){
+    document.getElementById('div1').style.display ='none';
+  }
+  function show2(){
+    document.getElementById('div1').style.display = 'block';
+  }
+
+
+
+
+
+
+  function validate_account() {
+    var amount = document.getElementById("amount").value;
+    // var re_enter_account_number = document.getElementById("re_enter_account_number").value;
+    // var ifsc_code = document.getElementById("ifsc_code").value;
+    if (amount < 10000 || amount === "") {
+        document.getElementById("error_account_number").style.display = "block";
+        document.getElementById("account_number").style.border = "1px solid red";
+    } else {
+        document.getElementById("error_account_number").style.display = "none";
+        document.getElementById("account_number").style.border = "";
+    }
+    // if (re_enter_account_number !== account_number || re_enter_account_number === "") {
+    //     document.getElementById("error_re_enter_account_number").style.display = "block";
+    //     document.getElementById("re_enter_account_number").style.border = "1px solid red";
+    // } else {
+    //     document.getElementById("error_re_enter_account_number").style.display = "none";
+    //     document.getElementById("re_enter_account_number").style.border = "";
+    // }
+    // if (ifsc_code === "") {
+    //     document.getElementById("error_ifsc_code").style.display = "block";
+    //     document.getElementById("ifsc_code").style.border = "1px solid red";
+    // } else {
+    //     document.getElementById("error_ifsc_code").style.display = "none";
+    //     document.getElementById("ifsc_code").style.border = "";
+    // }
+}
