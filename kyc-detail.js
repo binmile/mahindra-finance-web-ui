2ece7011f9308d4c8d88031e8fbd6571992dae7b
MONI <moni@binmile.com>
2023-08-03 12:47:00 +0530

    resolve drop down issues

--- kyc-detail.js
+++ kyc-detail.js
@@ -2,20 +2,10 @@ function toggleDropdown() {
     var dropdownOptions = document.getElementById("dropdown-options");
     var dropdownIcon = document.getElementById("dropdown-icon-occupation");
 
-    if (dropdownOptions.style.display === "none") {
-        dropdownOptions.style.display = "block";
-        dropdownIcon.classList.remove("fa-chevron-down");
-        dropdownIcon.classList.add("fa-chevron-up");
-        dropdownIcon.style.transform = "rotate(180deg)";
-    } else {
-        dropdownOptions.style.display = "none";
-        dropdownIcon.classList.remove("fa-chevron-up");
-        dropdownIcon.classList.add("fa-chevron-down");
-        dropdownIcon.style.transform = "rotate(0deg)";
-    }
+    dropdownOptions.style.display = "block";
+    dropdownIcon.style.transform = "rotate(180deg)";
 }
 
-// Function to handle selecting an option
 function selectOption(option) {
     const selectedOptionText = option.textContent.trim();
     const selectedOptionIcon = option.querySelector(".icon").classList.value;
@@ -28,11 +18,10 @@ function selectOption(option) {
 
     dropdownIcon.classList.remove("fa-chevron-up");
     dropdownIcon.classList.add("fa-chevron-down");
-    dropdownOptions.style.display = "none"; // Close the dropdown options
+    dropdownOptions.style.display = "none";
     dropdownIcon.style.transform = "rotate(0deg)";
 }
 
-// Attach click event listeners to the options
 const options = document.querySelectorAll(".custom-dropdown option");
 options.forEach(option => {
     option.addEventListener("click", function() {
