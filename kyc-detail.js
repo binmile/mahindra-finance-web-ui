ec790a73c4d45cbbdf271c5c7807330e31576d31
MONI <moni@binmile.com>
2023-08-03 08:43:17 +0530

    dropdown changes

--- kyc-detail.js
+++ kyc-detail.js
@@ -15,11 +15,11 @@ function toggleDropdown() {
 
 // Function to handle selecting an option
 function selectOption(option) {
-    var selectedOptionText = option.textContent.trim();
-    var selectedOptionIcon = option.querySelector(".icon").classList.value;
-    var dropdownInput = document.querySelector(".dropdown-input");
-    var dropdownIcon = document.getElementById("dropdown-icon");
-    var dropdownOptions = document.getElementById("dropdown-options");
+    const selectedOptionText = option.textContent.trim();
+    const selectedOptionIcon = option.querySelector(".icon").classList.value;
+    const dropdownInput = document.querySelector(".dropdown-input");
+    const dropdownIcon = document.getElementById("dropdown-icon");
+    const dropdownOptions = document.getElementById("dropdown-options");
 
     dropdownInput.value = selectedOptionText;
     dropdownInput.setAttribute("data-icon", selectedOptionIcon);
@@ -30,15 +30,9 @@ function selectOption(option) {
 }
 
 // Attach click event listeners to the options
-var options = document.querySelectorAll(".custom-dropdown option");
+const options = document.querySelectorAll(".custom-dropdown option");
 options.forEach(option => {
     option.addEventListener("click", function() {
         selectOption(this);
     });
-});
-
-// Event listener for the input field to toggle dropdown options visibility
-var dropdownInput = document.querySelector(".dropdown-input");
-dropdownInput.addEventListener("click", function() {
-    toggleDropdown();
 });
