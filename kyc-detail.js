df0ead6a3268fb2139ab8ad1de45b48068e486b0
MONI <moni@binmile.com>
2023-08-03 13:57:49 +0530

    drop down issues fixed

--- kyc-detail.js
+++ kyc-detail.js
@@ -1,5 +1,5 @@
-function toggleDropdown() {
-    var dropdownOptions = document.getElementById("dropdown-options");
+function toggleDropdownOccupation() {
+    var dropdownOptions = document.getElementById("dropdown-options-occupation");
     var dropdownIcon = document.getElementById("dropdown-icon-occupation");
 
     dropdownOptions.style.display = "block";
@@ -9,9 +9,9 @@ function toggleDropdown() {
 function selectOption(option) {
     const selectedOptionText = option.textContent.trim();
     const selectedOptionIcon = option.querySelector(".icon").classList.value;
-    const dropdownInput = document.querySelector(".dropdown-input");
+    const dropdownInput = document.querySelector(".drop-input-occupation");
     const dropdownIcon = document.getElementById("dropdown-icon-occupation");
-    const dropdownOptions = document.getElementById("dropdown-options");
+    const dropdownOptions = document.getElementById("dropdown-options-occupation");
 
     dropdownInput.value = selectedOptionText;
     dropdownInput.setAttribute("data-icon", selectedOptionIcon);
@@ -22,9 +22,44 @@ function selectOption(option) {
     dropdownIcon.style.transform = "rotate(0deg)";
 }
 
-const options = document.querySelectorAll(".custom-dropdown option");
+const options = document.querySelectorAll(".custom-dropdown-occupation option");
 options.forEach(option => {
     option.addEventListener("click", function() {
         selectOption(this);
+    });
+});
+
+
+
+
+
+function toggleDropdownannual() {
+    var dropdownOptions = document.getElementById("dropdown-options-annual");
+    var dropdownIcon = document.getElementById("dropdown-icon-annual");
+
+    dropdownOptions.style.display = "block";
+    dropdownIcon.style.transform = "rotate(180deg)";
+}
+
+function selectOptionAnnual(option) {
+    const selectedOptionText = option.textContent.trim();
+    const selectedOptionIcon = option.querySelector(".icon").classList.value;
+    const dropdownInput = document.querySelector(".drop-input-annual");
+    const dropdownIcon = document.getElementById("dropdown-icon-annual");
+    const dropdownOptions = document.getElementById("dropdown-options-annual");
+
+    dropdownInput.value = selectedOptionText;
+    dropdownInput.setAttribute("data-icon", selectedOptionIcon);
+
+    dropdownIcon.classList.remove("fa-chevron-up");
+    dropdownIcon.classList.add("fa-chevron-down");
+    dropdownOptions.style.display = "none";
+    dropdownIcon.style.transform = "rotate(0deg)";
+}
+
+const optionsannual = document.querySelectorAll(".custom-dropdown-annual option");
+optionsannual.forEach(option => {
+    option.addEventListener("click", function() {
+        selectOptionAnnual(this);
     });
 });
