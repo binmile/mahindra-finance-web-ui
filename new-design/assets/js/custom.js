e326c873522af7273da4b12f8c70db55024b04e9
manishk000 <manish.k@binmile.com>
2023-12-06 10:54:28 +0530

    animate done

--- new-design/assets/js/custom.js
+++ new-design/assets/js/custom.js
@@ -100,3 +100,44 @@ function handleClickRadio(ele1, ele2) {
     }
 }
 
+/*************************OTP Modal Start******************************/
+
+function handleClickOTP(){
+    var myModal = new bootstrap.Modal(document.getElementById("myOTPModal"));
+    myModal.show();
+}
+
+const inputs = document.getElementById("inputs"); 
+  
+inputs.addEventListener("input", function (e) { 
+    const target = e.target; 
+    const val = target.value; 
+  
+    if (isNaN(val)) { 
+        target.value = ""; 
+        return; 
+    } 
+  
+    if (val != "") { 
+        const next = target.nextElementSibling; 
+        if (next) { 
+            next.focus(); 
+        } 
+    } 
+}); 
+  
+inputs.addEventListener("keyup", function (e) { 
+    const target = e.target; 
+    const key = e.key.toLowerCase(); 
+  
+    if (key == "backspace" || key == "delete") { 
+        target.value = ""; 
+        const prev = target.previousElementSibling; 
+        if (prev) { 
+            prev.focus(); 
+        } 
+        return; 
+    } 
+});
+
+/*************************OTP Modal End******************************/
