07c434cb4b91ed4b42c0e8c54b720426468432e9
manishk000 <manish.k@binmile.com>
2023-12-28 16:13:01 +0530

    chart UI changes done

--- new-design/assets/js/graph.js
+++ new-design/assets/js/graph.js
@@ -1,29 +1,241 @@
 /*****************Graph Start***************/
-window.onload = function () {
-    var chart = new CanvasJS.Chart("chartContainer", {
-        animationEnabled: true,
-        title: {
-            text: "Growth chart"
-        },
-        axisY: {
-            title: "",
-            valueFormatString: "#0,,.",
-            suffix: "k",
-            prefix: "₹"
-        },
-        data: [{
-            type: "splineArea",
-            color: "rgba(34, 197, 94, 1)",
-            markerSize: 5,
-            xValueFormatString: "YYYY",
-            yValueFormatString: "₹#,##0.##",
-            dataPoints: [
-                { x: new Date(2000, 0), y: 55000 },
-                { x: new Date(2001, 0), y: 70000 },
-                { x: new Date(2002, 0), y: 73605 }
-            ]
-        }]
-    });
-    chart.render();
-}
+// window.onload = function () {
+//     var chart = new CanvasJS.Chart("chartContainer", {
+//         animationEnabled: true,
+//         title: {
+//             text: "Growth chart"
+//         },
+//         axisY: {
+//             title: "",
+//             valueFormatString: "#0,,.",
+//             suffix: "k",
+//             prefix: "₹"
+//         },
+//         data: [{
+//             type: "splineArea",
+//             color: "rgba(34, 197, 94, 1)",
+//             markerSize: 5,
+//             xValueFormatString: "YYYY",
+//             yValueFormatString: "₹#,##0.##",
+//             dataPoints: [
+//                 { x: new Date(2000, 0), y: 55000 },
+//                 { x: new Date(2001, 0), y: 70000 },
+//                 { x: new Date(2002, 0), y: 73605 }
+//             ]
+//         }]
+//     });
+//     chart.render();
+// }
 /*****************Graph End***************/
+
+
+const ctx = document.getElementById('myChart').getContext('2d');
+const DISPLAY = true;
+const BORDER = true;
+const CHART_AREA = true;
+const TICKS = true;
+/***********************tooltip Start*******************************/
+const getOrCreateTooltip = (chart) => {
+  let tooltipEl = chart.canvas.parentNode.querySelector('div');
+
+  if (!tooltipEl) {
+    tooltipEl = document.createElement('div');
+    tooltipEl.style.background = 'rgba(255, 255, 255, 1)';
+    tooltipEl.style.borderRadius = '6px';
+    tooltipEl.style.color = '#6B7280';
+    tooltipEl.style.opacity = 1;
+    tooltipEl.style.pointerEvents = 'none';
+    tooltipEl.style.position = 'absolute';
+    tooltipEl.style.transform = 'translate(-50%, 0)';
+    tooltipEl.style.transition = 'all .1s ease';
+    tooltipEl.style.border = '1.28px solid #F9F9F9';
+    tooltipEl.style.boxShadow = '0px 1px 2px 0px #0000000F, 0px 1px 3px 0px #0000001A';
+
+    const table = document.createElement('table');
+    table.style.margin = '0px';
+
+    tooltipEl.appendChild(table);
+    chart.canvas.parentNode.appendChild(tooltipEl);
+  }
+
+  return tooltipEl;
+};
+
+const externalTooltipHandler = (context) => {
+  const {chart, tooltip} = context;
+  const tooltipEl = getOrCreateTooltip(chart);
+
+  if (tooltip.opacity === 0) {
+    tooltipEl.style.opacity = 0;
+    return;
+  }
+
+  if (tooltip.body) {
+    const titleLines = tooltip.title || [];
+    const bodyLines = tooltip.body.map(b => b.lines);
+
+    const tableHead = document.createElement('thead');
+
+    titleLines.forEach(title => {
+      const tr = document.createElement('tr');
+      tr.style.borderWidth = 0;
+
+      const th = document.createElement('th');
+      th.style.borderWidth = 0;
+      th.style.fontSize = '14px';
+      th.style.fontWeight = '400';
+
+      const text = document.createTextNode(title);
+
+      th.appendChild(text);
+      tr.appendChild(th);
+      tableHead.appendChild(tr);
+    });
+
+    const tableBody = document.createElement('tbody');
+    bodyLines.forEach((body, i) => {
+      const colors = tooltip.labelColors[i];
+
+      const span = document.createElement('span');
+      span.style.background = colors.backgroundColor;
+      span.style.borderColor = colors.borderColor;
+      span.style.borderWidth = '2px';
+      span.style.marginRight = '10px';
+      span.style.height = '10px';
+      span.style.width = '10px';
+      span.style.display = 'inline-block';
+
+      const tr = document.createElement('tr');
+      tr.style.backgroundColor = 'inherit';
+      tr.style.borderWidth = 0;
+      tr.style.display = 'flex';
+      tr.style.justifyContent = 'center';
+      tr.style.color = '#242424';
+      tr.style.fontSize = '14px';
+      tr.style.fontWeight = '500';
+
+      const td = document.createElement('td');
+      const td2 = document.createElement('td');
+      td.style.borderWidth = 0;
+      td2.style.borderWidth = 0;
+
+      const text = document.createTextNode(body);
+      const rupeeSymbol = document.createTextNode("₹");
+      const k = document.createTextNode("K");
+
+      td.appendChild(text);
+      td2.appendChild(rupeeSymbol);
+      td.appendChild(k);
+      tr.appendChild(td2);
+      tr.appendChild(td);
+      tableBody.appendChild(tr);
+    });
+
+    const tableRoot = tooltipEl.querySelector('table');
+
+    while (tableRoot.firstChild) {
+      tableRoot.firstChild.remove();
+    }
+
+    tableRoot.appendChild(tableHead);
+    tableRoot.appendChild(tableBody);
+  }
+
+  const {offsetLeft: positionX, offsetTop: positionY} = chart.canvas;
+
+  tooltipEl.style.opacity = 1;
+  tooltipEl.style.left = positionX + tooltip.caretX + 'px';
+  tooltipEl.style.top = positionY + tooltip.caretY + 'px';
+  tooltipEl.style.font = tooltip.options.bodyFont.string;
+  tooltipEl.style.padding = tooltip.options.padding + 'px ' + tooltip.options.padding + 'px';
+};
+/***********************tooltip End*******************************/
+
+const myChart = new Chart(ctx, {
+    type: 'line',
+    data: {
+        labels: ['At investment','', 'Today', '', 'On maturity'],
+        datasets: [{
+            data: [55, 57, 70, 68, 73],
+            pointRadius: 4,
+            backgroundColor: (context) => {
+              const bgColor = [
+                'rgba(0, 255, 26, 0.6)',
+                'rgba(0, 255, 26, 0.2)',
+                'rgba(0, 255, 26, 0)'
+              ];
+              if(!context.chart.chartArea){
+                return;
+              }
+              const {ctx, data, chartArea:{top, bottom}} = context.chart;
+              const gradientBg = ctx.createLinearGradient(0, top, 0, bottom);
+              const colorTranches = 1 / (bgColor.length - 1);
+              for (let i = 0; i < bgColor.length; i++){
+                gradientBg.addColorStop(0 + i * colorTranches, bgColor[i])
+              }
+              return gradientBg;
+            },
+            borderColor: [
+                'rgba(34, 197, 94, 1)',
+                'rgba(34, 197, 94, 1)',
+                'rgba(34, 197, 94, 1)'
+            ],
+            borderWidth: 4,
+            fill: true
+        }]
+    },
+    options: {
+        responsive: true,
+        plugins: {
+          title: {
+            display: true,
+            text: 'Growth chart',
+            align: 'start',
+            padding: {
+                bottom: 18
+            },
+            font: {
+                size: 16,
+                weight: 'bold'
+            },
+            color: '#231F20'
+          },
+          legend: false,
+          tooltip: {
+            enabled: false,
+            external: externalTooltipHandler
+          }
+        },
+        tension: 0.4,
+        scales: {
+             x: {
+                  grid:{
+                        color: '#ADADAD',
+                    },
+                    border: {
+                        dash: [2,4],
+                    },
+                },
+              y: {
+                beginAtZero: true,
+                border: {
+                  display: false
+                },
+                grid: {
+                  color: function(context) {
+                    if (context.tick.value == 0) {
+                      return '#ADADAD' ;
+                    }
+                    return '';
+                  },
+                },
+                ticks: {
+                    stepSize: 30,
+                    callback: function(value, index, ticks) {
+                        return '₹ ' + value + 'K';
+                    }
+                }
+              }
+        }
+    }
+});
